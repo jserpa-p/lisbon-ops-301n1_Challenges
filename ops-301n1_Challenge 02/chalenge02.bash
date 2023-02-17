46e245ded3a12c36580f524a04009e804d8f6bca
jserpa-p <79847245+jserpa-p@users.noreply.github.com>
2023-02-17 15:41:52 +0000

    Update chalenge02.bash

--- ops-301n1_Challenge 02/chalenge02.bash
+++ ops-301n1_Challenge 02/chalenge02.bash
@@ -1,9 +1,13 @@
 #!/bin/bash/
 
-#Now I want to copy the system log to my current working directory
-
+#Firstly I need to create the variable. Something that tells me the current date and time
 data_hora=$(date +"day"%d"month"%m"year"%y_%T)
+
+#This one is to print the variable
 echo $data_hora
+
+#Now I want to copy the system log to my current working directory. 
+#Note that this was made in kali linux so the logs were in a different place
 cp -r /var/log/journal ./journal_$data_hora
 
 
