7f614981cc1c98e0b4f7b7745e38b0a9fbe567d6
jserpa-p <79847245+jserpa-p@users.noreply.github.com>
2023-02-21 17:01:05 +0000

    Delete chalenge02.bash

--- chalenge02.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash/
-
-#Now I want to copy the system log to my current working directory
-
-data_hora=$(date +"day"%d"month"%m"year"%y_%T)
-echo $data_hora
-cp -r /var/log/journal ./journal_$data_hora
-
-
