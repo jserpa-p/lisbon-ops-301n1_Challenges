1287d8aab0352bb11d0db2921f65ff70d5a00a71
jserpa-p <79847245+jserpa-p@users.noreply.github.com>
2023-02-22 17:37:48 +0000

    Update challenge_03.sh

--- ops-301n1_Challenge 03/challenge_03.sh
+++ ops-301n1_Challenge 03/challenge_03.sh
@@ -1,13 +1,19 @@
 #!/bin/bash/
 
-#This is to let the user choose the path he wants
-
+#Firstly I let the user know what are the existing directories
+ls
+sleep 1
+#This is to let the user choose the path he wants and I save the awnser in a variable
 echo "Choose the directory path you want: "
 read filepath
+#Then I change directory with 'cd'
 cd $filepath
+#This is to let the user choose the permissions and I save the awnser in a variable
 echo "Choose your permissions(numbers ex:777): "
 read perm 
+#Then I change all files to the permissions that the user wants with 'chmod'
 chmod -R "$perm" *
+#Finally I show to the user the changed directories and files
 ls -la
 sleep 3
 
